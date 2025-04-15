9e30c4a4f740474b15b0bd9fec060bc0a5b3d2bc
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2025-04-15 16:42:23 +0300

    change endpoint

--- src/services/httpServices.ts
+++ src/services/httpServices.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { Meme, UpdateMeme } from '../types';
 
 export const getData = async (): Promise<Meme[]> => {
-	const { data } = await axios(import.meta.env.VITE_BASE_URL);
+	const { data } = await axios(`${import.meta.env.VITE_BASE_URL}/memes`);
 	return data;
 };
 
 export const updateMeme = async ({ id, body }: { id: string; body: UpdateMeme }) => {
 	const { data } = await axios({
-		url: `${import.meta.env.VITE_BASE_URL}/${id}`,
+		url: `${import.meta.env.VITE_BASE_URL}/memes/${id}`,
 		method: 'PATCH',
 		data: body,
 	});
